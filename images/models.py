51e7f8d999ab66fb7999aba013b23bd40144ba59
Omid Hashemzadeh <omidh2015@gmail.com>
2025-06-29 23:01:41 +0100

    fix image upload

--- images/models.py
+++ images/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+from core.azure_storage import AzureMediaStorage
+
 
 class Image(models.Model):
     title = models.CharField(max_length=128)
-    file = models.ImageField(upload_to='images/')
+    file = models.ImageField(
+        storage=AzureMediaStorage(),
+        upload_to='images/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
